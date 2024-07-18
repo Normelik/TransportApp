3d34ba34925e15c2d6f6fd7b1043b3535b5bd709
Matouš Švec <136631267+Normelik@users.noreply.github.com>
2024-07-18 09:34:59 +0200

    added health controller to check if the backend is running

---
+++ backend/src/main/java/com/transport/backend/controllers/HealthController.java
@@ -0,0 +1,14 @@
+package com.transport.backend.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<?> health(){
+        return ResponseEntity.ok().body("Backend is running");
+    }
+}
