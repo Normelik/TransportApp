342530bdbfee286f39902b116c021a1cccac1ae2
Matouš Švec <136631267+Normelik@users.noreply.github.com>
2025-01-22 17:47:30 +0100

    anotation for warehouse values

--- backend/src/main/java/com/simple/backend/models/Warehouse.java
+++ backend/src/main/java/com/simple/backend/models/Warehouse.java
@@ -1,6 +1,7 @@
 package com.simple.backend.models;
 
 import jakarta.persistence.*;
+import lombok.NonNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,10 +12,13 @@ public class Warehouse {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(name = "warehouse_name")
     private String name;
+    @Column(name = "unloading_place")
+    @NonNull
     private String unloadingPlace;
     @OneToMany
-    private List<Shipment> plannedShipments;
+    private List<Shipment> plannedShipments = new ArrayList<>(12);
     private String owner;
     private String supplier;
 }
